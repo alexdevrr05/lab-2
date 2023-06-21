b71fca619e5b03d153d11b0ea5f4e3f78777402e
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2023-06-20 18:02:39 -0600

    se mejora el diseño ui

--- js/material/material-view.js
+++ js/material/material-view.js
@@ -1,4 +1,10 @@
 let title = document.getElementById('title');
+let btnform = document.getElementById('btnform');
+let nombre = document.getElementById('nombre');
+let cantidad = document.getElementById('cantidad');
+let volumen = document.getElementById('volumen');
+let unidad = document.getElementById('unidad');
+let imagen = document.getElementById('imagen');
 
 window.addEventListener('DOMContentLoaded', () => {
   const updateTable = (data) => {
@@ -44,3 +50,64 @@ window.addEventListener('DOMContentLoaded', () => {
     updateTable(data);
   });
 });
+
+// Validar que los campos no vengan vacíos
+const isValidForm = () => {
+  const nombreValue = nombre.value.trim();
+  const cantidadValue = cantidad.value.trim();
+  const volumenValue = volumen.value.trim();
+  const unidadValue = unidad.value.trim();
+  const imagenValue = imagen.value.trim();
+
+  if (
+    nombreValue === '' ||
+    cantidadValue === '' ||
+    volumenValue === '' ||
+    unidadValue === '' ||
+    imagenValue === ''
+  ) {
+    alert('Por favor, completa todos los campos');
+    return false;
+  }
+
+  return true;
+};
+
+btnform.addEventListener('click', async (e) => {
+  e.preventDefault(); // Evitar el evento por defecto del botón
+  if (isValidForm()) {
+    addProductRenderer();
+  }
+});
+
+const addProductRenderer = async () => {
+  const objMaterial = {
+    nombre: nombre.value,
+    cantidad: cantidad.value,
+    volumen: volumen.value,
+    unidad: unidad.value,
+    imagen: imagen.files[0].path,
+  };
+
+  await window.electronAPI.addMaterial(objMaterial);
+
+  // Limpiar todos los campos
+  clearinput();
+
+  // Petición a MySQL para obtener los datos actualizados
+  window.electronAPI.executeQuery('SELECT * FROM material', (error, data) => {
+    if (error) {
+      console.error('Error al ejecutar la consulta:', error);
+    } else {
+      updateTable(data); // Actualizar la tabla con los nuevos datos
+    }
+  });
+};
+
+const clearinput = () => {
+  nombre.value = '';
+  cantidad.value = '';
+  volumen.value = '';
+  unidad.value = '';
+  imagen.value = '';
+};
